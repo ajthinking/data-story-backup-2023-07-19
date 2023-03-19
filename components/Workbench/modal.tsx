d0a5dd30db135dc162397bba07afa8211cb6c3da
Anders Jurisoo <jurisoo@hotmail.com>
2023-03-19 12:16:41 +0100

    Allow children in Modal

--- components/Workbench/modal.tsx
+++ components/Workbench/modal.tsx
@@ -4,24 +4,19 @@ export function Modal({
   setShowModal,
   title,
   content,
+  children,
 }: any) {
   useEscapeKey(() => setShowModal(false));
 
   return <>
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="relative w-auto my-6 mx-auto px-8">
-              {
-          /*content*/
-        }
+            <div className="relative w-auto my-8 mx-auto px-8">
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                {
-            /*header*/
-          }
-                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-xl font-semibold">
+                <div className="flex items-start justify-between px-8 py-2 uppercase border-solid border-slate-200 rounded-t">
+                  <div className="mt-4 flex flex-col align-center justify-middleitems-center justify-center text-base text-gray-400 font-bold tracking widest">
                     {title}
-                  </h3>
-                  <button className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none" onClick={() => setShowModal(false)}>
+                  </div>
+                  <button className="p-1 ml-auto bg-transparent text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none" onClick={() => setShowModal(false)}>
                     <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
                       ×
                     </span>
@@ -30,10 +25,11 @@ export function Modal({
                 {
             /*body*/
           }
-                <div className="relative p-6 flex-auto">
+                <div className="relative pb-6 pt-4 px-6 flex-auto space-y-1">
                   <p className="my-4 text-slate-500 text-sm leading-relaxed">
-                    {content}
+                    {content || null}
                   </p>
+                  {children}
                 </div>
                 {
             /*footer*/
