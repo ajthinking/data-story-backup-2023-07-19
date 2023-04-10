be97d4efbd5004860de7ccd22334ffe73b4b317a
Anders Jurisoo <jurisoo@hotmail.com>
2023-04-10 13:01:57 +0200

    Todo on ListFiles test

--- core/computers/ListFiles.test.ts
+++ core/computers/ListFiles.test.ts
@@ -3,7 +3,7 @@ import { RunArgs } from "../Computer";
 import { when } from "../support/computerTester/ComputerTester";
 import { ListFiles } from "./ListFiles";
 
-it('does something', async () => {
+it.todo('does something', async () => {
   await when(ListFiles)
     .hasDefaultParams()
     .getsInput([1, 2])
