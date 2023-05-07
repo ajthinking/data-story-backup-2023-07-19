ec1752a1892b1f920557ec4c4158619467ea4a1f
Anders Jurisoo <jurisoo@hotmail.com>
2023-05-07 10:06:28 +0200

    Non forced opening of node settings

--- components/Workbench/Workbench.tsx
+++ components/Workbench/Workbench.tsx
@@ -32,20 +32,26 @@ export default function Workbench() {
 
   const [showConfigModal] = useState(false);
   const [showRunModal, setShowRunModal] = useState(false);
-  const [showAddNodeModal, setShowAddNodeModal] = useState(false);
+  const [showAddNodeModal, setShowAddNodeModal] = useState(false);  
 
   // MOVE OUT
   useEffect(() => {
+    // restore enterkey event listener
+    if(!showAddNodeModal) {
+      console.log("Restoring enter key event listener")
+      window.addEventListener("keydown", handleEnterPress);
+    }
+
     function handleKeyPress(event: KeyboardEvent) {
       // Swedish Mac keyboard 🤷‍♂️
       const shiftR = event.shiftKey && event.code === "KeyR";
       const shiftPlus = event.shiftKey && event.code === "Minus"
-
+  
       const arrowUp = event.code === "ArrowUp";
       const arrowDown = event.code === "ArrowDown";
       const arrowLeft = event.code === "ArrowLeft";
       const arrowRight = event.code === "ArrowRight";
-
+  
       const enter = event.code === "Enter";
       
       // Ensure no modal is already open
@@ -55,49 +61,80 @@ export default function Workbench() {
         showRunModal,
         showAddNodeModal,
       ].find(Boolean)) return;
-
+  
       // Open modal!
       if (shiftR) setShowRunModal(true);
-      if (shiftPlus) setShowAddNodeModal(true);
-
+      if (shiftPlus) {
+        // When opening the add node modal, we want to disable the enter key
+        console.log("Disabling enter key event listener")        
+        window.removeEventListener("keydown", handleEnterPress);
+        setShowAddNodeModal(true);
+      }
+  
       // Open node settings modal
       const openable = (() => {
         const selectedNodes = nodes.filter((node) => node.selected);
         const one = selectedNodes.length === 1;
         if(!one) return null;
-
+  
         return selectedNodes[0];
       })()
-
+  
       console.log({
+        method: 'handleKeyPress',
         enter,
         openable,
         showAddNodeModal,
       })
-      
-      if (enter && openable && !showAddNodeModal) setOpenNodeModalId(openable.id);
-
+  
       // Select nodes
       if (arrowUp) traverseNodes("up");
       if (arrowDown) traverseNodes("down");
       if (arrowLeft) traverseNodes("left");
       if (arrowRight) traverseNodes("right");
     }
+  
+    function handleEnterPress(event: KeyboardEvent) {
+      // Swedish Mac keyboard 🤷‍♂️
+  
+      const enter = event.code === "Enter";
+      
+      // Ensure no modal is already open
+      if ([
+        openNodeModalId,
+        showConfigModal,
+        showRunModal,
+        showAddNodeModal,
+      ].find(Boolean)) return;
+  
+      // Open node settings modal
+      const openable = (() => {
+        const selectedNodes = nodes.filter((node) => node.selected);
+        const one = selectedNodes.length === 1;
+        if(!one) return null;
+  
+        return selectedNodes[0];
+      })()
+      
+      if (enter && openable && !showAddNodeModal) setOpenNodeModalId(openable.id);
+    }
 
     // Add the event listener when the component mounts
     window.addEventListener("keyup", handleKeyPress);
+    window.addEventListener("keydown", handleEnterPress);
 
     // Remove the event listener when the component unmounts
     return () => {
       window.removeEventListener("keyup", handleKeyPress);
+      window.removeEventListener("keydown", handleEnterPress);
     };
   }, [
     nodes,
     openNodeModalId,
+    setOpenNodeModalId,
     showConfigModal,
     showRunModal,
     showAddNodeModal,
-    setOpenNodeModalId,
     traverseNodes,
   ]);
   
